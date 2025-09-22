90b207543546ba13c2093e99338e76635125990d
SpartinQwerty <wleggemann@hotmail.com>
2025-09-21 21:45:36 -0400

    feat - SandwichMaker class to check and make sandwich

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -4,8 +4,15 @@ class SandwichMaker:
         self.machine_resources = resources
 
     def check_resources(self, ingredients):
-        """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        """Check if there are enough resources for the sandwich."""
+        for item in ingredients:
+            if ingredients[item] > self.machine_resources[item]:
+                print(f"Sorry, not enough {item}.")
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        """Remove ingredients from resources after making a sandwich."""
+        for item in order_ingredients:
+            self.machine_resources[item] -= order_ingredients[item]
+        print(f"Here is your {sandwich_size} sandwich.")
